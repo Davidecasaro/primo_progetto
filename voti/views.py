84576d7df2913574179cd62f71dd173cf9907966
Davidecasaro <davide.casaro@alessandrinimainardi.edu.it>
2023-12-22 08:10:14 +0100

    finish

--- voti/views.py
+++ voti/views.py
@@ -14,16 +14,9 @@ def wiew_b(request):
     }
     return render(request,"wiew_b.html", context)
 
-def wiew_c(request):
-    somma=0
-    media=0
-    for(stud,dati in voti.items): 
-        for(mat,voto,assenze in dati):
-            somma+=voto
-            media=somma/5
-          
+def wiew_c(request):     
     context={
-        'media' : media
+        'media' : media,
         'voti' : {'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7,4),("Storia",7,4),("Geografia",5,7)],
            'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9,0),("Storia",8,2),("Geografia",8,1)],
            'Nicola Spina':[("Matematica",7,2),("Italiano",6,2),("Inglese",4,3),("Storia",8,2),("Geografia",8,2)]}
@@ -31,35 +24,6 @@ def wiew_c(request):
     return render(request,"wiew_c.html", context)
 
 def wiew_d(request):
-    max={{max}}
-    maxMat={{maxMat}}
-    maxStud={{maxStud}}
-    min={{min}}
-    minMat={{minMat}}
-    minStd={{minStud}}
-    voti={{voti}}
-
-    for(stud,dati in voti.items):
-        for(i < 5):
-            if(voti[stud][i][1] < min){
-                min=voti[stud][i][1]
-                minMat=voti[stud][i][0]
-                minStud=stud
-            }
-            if(voti[stud][i][1] == min){
-                minMat+=voti[stud][i][0]
-                minStud+=stud
-            }
-            if(voti[stud][i][1] > max){
-                max=voti[stud][i][1]
-                minMat=voti[stud][i][0]
-                minStud=stud
-            }
-            if(voti[stud][i][1] == max){
-                maxMat+=voti[stud][i][0]
-                maxStud+=stud
-            }
-        
     context={
         'voti' : {'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7,4),("Storia",7,4),("Geografia",5,7)],
            'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9,0),("Storia",8,2),("Geografia",8,1)],
