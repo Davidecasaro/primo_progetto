34ad6b98bcadbc982bdccea5198e3b1b230bafb7
Davidecasaro <davide.casaro@alessandrinimainardi.edu.it>
2023-12-22 09:54:03 +0100

    finish

--- voti/views.py
+++ voti/views.py
@@ -16,7 +16,7 @@ def wiew_b(request):
 
 def wiew_c(request):     
     context={
-        'media' : media
+        'media' : media,
         'voti' : {'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7,4),("Storia",7,4),("Geografia",5,7)],
            'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9,0),("Storia",8,2),("Geografia",8,1)],
            'Nicola Spina':[("Matematica",7,2),("Italiano",6,2),("Inglese",4,3),("Storia",8,2),("Geografia",8,2)]}
